19a0cda90c968c34d8b7dfc88f4e93c2cccb83de
Murilo Eberhardt <murilo.05eberhardt@gmail.com>
2025-09-20 14:07:38 -0300

    Criação da main

--- src/main/java/app/padariapaodoce/PadariapaodoceApplication.java
+++ src/main/java/app/padariapaodoce/PadariapaodoceApplication.java
@@ -1,4 +1,12 @@
 package app.padariapaodoce;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PadariapaodoceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadariapaodoceApplication.class, args);
+    }
 }
