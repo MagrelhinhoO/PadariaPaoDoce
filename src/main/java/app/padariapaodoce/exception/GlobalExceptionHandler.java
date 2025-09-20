2fc8735c84d7b141e78c00a25193b3c1433aa445
Murilo Eberhardt <murilo.05eberhardt@gmail.com>
2025-09-20 14:07:15 -0300

    Criação do GlobalExceptionHandler

--- src/main/java/app/padariapaodoce/exception/GlobalExceptionHandler.java
+++ src/main/java/app/padariapaodoce/exception/GlobalExceptionHandler.java
@@ -1,4 +1,30 @@
 package app.padariapaodoce.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> erros = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                erros.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> erro = new HashMap<>();
+        erro.put("erro", ex.getMessage());
+        return new ResponseEntity<>(erro, HttpStatus.BAD_REQUEST);
+    }
 }
